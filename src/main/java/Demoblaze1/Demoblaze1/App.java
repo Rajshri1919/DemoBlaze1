a4557780f94c39af189df3f325550951b399bea9
Rajshri1919 <rajshripawar1919@gmail.com>
2023-10-11 12:03:46 -0400

    hello first demoblaze project

---
+++ src/main/java/Demoblaze1/Demoblaze1/App.java
@@ -0,0 +1,13 @@
+package Demoblaze1.Demoblaze1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Demoblaze1/Demoblaze1/AppTest.java
@@ -0,0 +1,20 @@
+package Demoblaze1.Demoblaze1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
